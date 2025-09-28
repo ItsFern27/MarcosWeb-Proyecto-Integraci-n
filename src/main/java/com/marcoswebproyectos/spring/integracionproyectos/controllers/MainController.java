7620d2cefba1e15da74beaff2367e2495b2cfc23
ItsFern27 <fjarevalo07@gmail.com>
2025-09-28 17:26:35 -0500

    Actualizacion Springboot

--- src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controllers/MainController.java
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controllers/MainController.java
@@ -1,14 +1,23 @@
 package com.marcoswebproyectos.spring.integracionproyectos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class MainController {
     
-    @GetMapping("/")
-    public String getIndex() {
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model) {
+        
+        // Adición para gestión dinámica de datos de los proyectos
+        model.addAttribute("Titulo1", "Dashboard Analytics");
+        model.addAttribute("Titulo2", "App de Fitness");
+        model.addAttribute("Titulo3", "Sistema de Seguridad");
+        model.addAttribute("Titulo4", "E-commerce Platform");
+        model.addAttribute("Titulo5", "ML Predictor");
+        model.addAttribute("Titulo6", "Red Social Creativa");
         return "index";
     }
     
--- src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controllers/ProyectosController.java
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controllers/ProyectosController.java
@@ -11,6 +11,5 @@ public class ProyectosController {
     public String giveProys() {
         return "mis-proyectos";
     }
-    
-    
+        
 }
