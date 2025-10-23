f29b3945ea5707e11f2679439e1830fa6a747aaf
Crismar12 <printza9.08@gmail.com>
2025-10-23 11:09:04 -0500

    conexion a supabase

---
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package com.marcoswebproyectos.spring.integracionproyectos.controller;
+
+import com.marcoswebproyectos.spring.integracionproyectos.model.Usuario;
+import com.marcoswebproyectos.spring.integracionproyectos.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    private final UsuarioRepository repo;
+
+    public UsuarioController(UsuarioRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/usuarios")
+    public List<Usuario> getUsuarios() {
+        return repo.findAll();
+    }
+}
