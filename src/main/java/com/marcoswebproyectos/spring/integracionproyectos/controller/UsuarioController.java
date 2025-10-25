04ad79b9c9e4fcbec7f9fdf967c5e3234b7c981d
sergio <amayasergio1310@gmail.com>
2025-10-25 00:36:55 -0500

    actualizacion en put y delete en UsuarioController

--- src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controller/UsuarioController.java
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/controller/UsuarioController.java
@@ -2,8 +2,12 @@ package com.marcoswebproyectos.spring.integracionproyectos.controller;
 
 import com.marcoswebproyectos.spring.integracionproyectos.model.Usuario;
 import com.marcoswebproyectos.spring.integracionproyectos.repository.UsuarioRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,24 @@ public class UsuarioController {
     @PostMapping("/usuarios")
     public Usuario createUsuario(@RequestBody Usuario usuario) {
         return repo.save(usuario);
+    }
+
+    @PutMapping("/usuarios/{id}")
+    public Usuario updateUsuario(@PathVariable Long id, @RequestBody Usuario usuarioActualizado) {
+        return repo.findById(id)
+                .map(usuario -> {
+                    usuario.setNombre(usuarioActualizado.getNombre());
+                    usuario.setEmail(usuarioActualizado.getEmail());
+                    usuario.setPassword(usuarioActualizado.getPassword());
+                    return repo.save(usuario);
+                })
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con ID: " + id));
+    }
+
+    
+    @DeleteMapping("/usuarios/{id}")
+    public String deleteUsuario(@PathVariable Long id) {
+        repo.deleteById(id);
+        return "Usuario eliminado con éxito (ID: " + id + ")";
     }
 }
