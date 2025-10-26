4ce7b9ef784a622d5ca54afc35df56ec9fde8c3d
ItsFern27 <fjarevalo07@gmail.com>
2025-10-26 17:38:35 -0500

    spring security

--- src/main/java/com/marcoswebproyectos/spring/integracionproyectos/model/Tickets_Soporte.java
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/model/Tickets_Soporte.java
@@ -7,9 +7,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tickets_soporte")
+@Getter
+@Setter
 public class Tickets_Soporte {
 
     @Id
