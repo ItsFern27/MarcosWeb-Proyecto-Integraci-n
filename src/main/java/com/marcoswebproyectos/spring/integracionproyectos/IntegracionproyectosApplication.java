89d8c5d59559ebd7c2ffc893ade39133f161858d
Crismar12 <printza9.08@gmail.com>
2025-10-28 14:15:23 -0500

    arreglo

--- src/main/java/com/marcoswebproyectos/spring/integracionproyectos/IntegracionproyectosApplication.java
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/IntegracionproyectosApplication.java
@@ -1,7 +1,14 @@
 package com.marcoswebproyectos.spring.integracionproyectos;
 
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.marcoswebproyectos.spring.integracionproyectos.model.Usuario;
+import com.marcoswebproyectos.spring.integracionproyectos.repository.UsuarioRepository;
 
 @SpringBootApplication
 public class IntegracionproyectosApplication {
