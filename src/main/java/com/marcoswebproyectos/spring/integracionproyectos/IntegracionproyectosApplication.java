7fe3c20ad7ab484edecd55dbe01199e038b3423d
ItsFern27 <fjarevalo07@gmail.com>
2025-11-15 18:37:43 -0500

    minor fixes

--- src/main/java/com/marcoswebproyectos/spring/integracionproyectos/IntegracionproyectosApplication.java
+++ src/main/java/com/marcoswebproyectos/spring/integracionproyectos/IntegracionproyectosApplication.java
@@ -1,21 +1,8 @@
 package com.marcoswebproyectos.spring.integracionproyectos;
 
-import java.util.List;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.marcoswebproyectos.spring.integracionproyectos.model.Usuario;
-import com.marcoswebproyectos.spring.integracionproyectos.repository.UsuarioRepository;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import java.util.List;
-import com.marcoswebproyectos.spring.integracionproyectos.model.Usuario;
-import com.marcoswebproyectos.spring.integracionproyectos.repository.UsuarioRepository;
 
 @SpringBootApplication
 public class IntegracionproyectosApplication {
